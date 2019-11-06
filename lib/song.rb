eb1edbb7761d99b0b126b17fe74c8efb30a76efb
Justin Phillips <justinphil02@yahoo.com>
2019-11-06 03:50:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,6 +11,11 @@ def save
   @@all << self
 end
 
+def artist_name=(name)
+#  @@all.detect {|artist_instance| artist_instance.name == artist_name} || new(artist_name)
+Artist.find_or_create_by_name(name)
+
+end 
 
 
 ## class methods
