083619139879c0bbf198266cd7912f42d181a978
Justin Phillips <justinphil02@yahoo.com>
2019-11-06 03:38:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,8 @@ end
 def new_by_filename(filename)
 split_array = filename.split(" - ")
 new_song = Song.new(split_array[1])
-Artist.find_or_create_by_name(split_array[0]).add_song(new_song)
+
+new_song.artist(find_or_create_by_name(split_array[0]))
 end
 
 ## class methods
