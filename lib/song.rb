17104e9da1b7502f62d00e832c1d244a573471bc
Justin Phillips <justinphil02@yahoo.com>
2019-11-06 03:41:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,18 +11,21 @@ def save
   @@all << self
 end
 
-def new_by_filename(filename)
-split_array = filename.split(" - ")
-new_song = Song.new(split_array[1])
 
-new_song.artist = Artist.find_or_create_by_name(split_array[0])
-end
 
 ## class methods
 def self.all
   @@all
 end
 
+def self.new_by_filename(filename)
+split_array = filename.split(" - ")
+new_song = Song.new(split_array[1])
+
+new_song.artist = Artist.find_or_create_by_name(split_array[0])
+end
 
 
 end #end of class
+
+
