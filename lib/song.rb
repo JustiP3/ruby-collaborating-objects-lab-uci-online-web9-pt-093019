4e01342c52cc1402ddedacab1cea5b7096d7eae9
Justin Phillips <justinphil02@yahoo.com>
2019-11-06 03:40:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ def new_by_filename(filename)
 split_array = filename.split(" - ")
 new_song = Song.new(split_array[1])
 
-new_song.artist = find_or_create_by_name(split_array[0])
+new_song.artist = Artist.find_or_create_by_name(split_array[0])
 end
 
 ## class methods
